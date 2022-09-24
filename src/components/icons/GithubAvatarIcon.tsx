cd9356bfa946e3ef453226b28789274ee2db67aa
NightFeather <leolee50910@gmail.com>
2022-09-24 16:06:31 +0800

    Update GithubAvatarIcon.tsx

--- src/components/icons/GithubAvatarIcon.tsx
+++ src/components/icons/GithubAvatarIcon.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { GITHUB_USER_ID } from '../../config';
 
 export default function GithubAvatarIcon() {
-  return (<img className="w-7 inline-block rounded-full group-hover:scale-[1.08] transition-transform duration-300" src={`https://avatars.githubusercontent.com/u/${GITHUB_USER_ID}?v=4`} width="40" height="40" alt="GitHub Avatar"/>);
+  return (<img className="w-7 inline-block rounded-full md:group-hover:scale-[1.08] transition-transform duration-300" src={`https://avatars.githubusercontent.com/u/${GITHUB_USER_ID}?v=4`} width="40" height="40" alt="GitHub Avatar"/>);
 };
