c578a82495ae50c6c32d87ca4ac4858ea5d7547c
NightFeather <leolee50910@gmail.com>
2022-09-26 22:36:21 +0800

    add og thumbnail, change icon

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,5 @@
 
 export const SITE_TITLE = "NightFeather's Life Log";
 export const SITE_DESCRIPTION = '破破的部落格';
+export const SITE_THUMBNAIL = 'https://cdn.discordapp.com/attachments/1022749268039188540/1022962743894093904/agathe-2cdvYh6ULCs-unsplash.jpg';
 export const GITHUB_USER_ID = 77222233;
