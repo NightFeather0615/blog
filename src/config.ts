4f02aa46def531cf7e002a10fcaa280acaf36089
NightFeather <leolee50910@gmail.com>
2022-12-19 01:32:37 +0800

    update site info

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "NightFeather's Life Log";
-export const SITE_DESCRIPTION = '破破的部落格';
+export const SITE_TITLE = "NightFeather's Blog";
+export const SITE_DESCRIPTION = '「以夢為馬，不負韶華。」';
 export const SITE_THUMBNAIL = 'https://cdn.discordapp.com/attachments/1022749268039188540/1022962743894093904/agathe-2cdvYh6ULCs-unsplash.jpg';
 export const GITHUB_USER_ID = 77222233;
