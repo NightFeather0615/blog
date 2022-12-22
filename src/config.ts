387aa17184a134eec1bce73b57ef9f8af549c9ba
NightFeather <leolee50910@gmail.com>
2022-12-23 02:13:37 +0800

    webp

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@
 
 export const SITE_TITLE = "NightFeather's Blog";
 export const SITE_DESCRIPTION = '「以夢為馬，不負韶華。」';
-export const SITE_THUMBNAIL = 'https://cdn.discordapp.com/attachments/1022749268039188540/1022962743894093904/agathe-2cdvYh6ULCs-unsplash.jpg';
+export const SITE_THUMBNAIL = '/header-background.webp';
 export const GITHUB_USER_ID = 77222233;
