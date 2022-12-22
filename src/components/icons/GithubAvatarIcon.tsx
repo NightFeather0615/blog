7be820fb4d8ffb1ff437fa6e3d7c9d8dae8b4571
NightFeather <leolee50910@gmail.com>
2022-12-23 02:44:04 +0800

    webp

--- src/components/icons/GithubAvatarIcon.tsx
+++ src/components/icons/GithubAvatarIcon.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { GITHUB_USER_ID } from '../../config';
+import { Image } from '@astrojs/image/components';
 
 export default function GithubAvatarIcon() {
-  return (<img className="w-7 inline-block rounded-full md:group-hover:scale-[1.08] transition-transform duration-300" src={`https://avatars.githubusercontent.com/u/${GITHUB_USER_ID}?v=4`} width="40" height="40" alt="GitHub Avatar"/>);
+  return (<Image className="w-7 inline-block rounded-full md:group-hover:scale-[1.08] transition-transform duration-300" src={`https://avatars.githubusercontent.com/u/${GITHUB_USER_ID}?v=4`} format="webp" quality={100} width="40" height="40" alt="GitHub Avatar"/>);
 };
