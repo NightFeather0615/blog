329313cfcadbdfd34df48616faf8c8c3d82bd926
NightFeather <leolee50910@gmail.com>
2023-09-19 22:06:14 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "NightFeather's Blog";
-export const SITE_DESCRIPTION = '「以夢為馬，不負韶華。」';
+export const SITE_DESCRIPTION = 'All those moments will be lost in time, like tears in rain.';
 export const SITE_THUMBNAIL = '/header-background.webp';
 export const GITHUB_USER_ID = 77222233;
